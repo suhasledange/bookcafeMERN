6526aa7e65fb8137cfe23a6f48d1a1d477c6faee
suhasledange <suhasledange10@gmail.com>
2025-08-01 11:31:06 +0530

    review model added

---
+++ server/models/Review.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const reviewSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true
+  },
+  book: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Book",
+    required: true
+  },
+  rating: {
+    type: Number,
+    required: true,
+    min: 1,
+    max: 5
+  },
+  message: {
+    type: String,
+    maxlength: 1000
+  }
+}, { timestamps: true });
+
+const Review = mongoose.model("Review", reviewSchema);
+export default Review;
